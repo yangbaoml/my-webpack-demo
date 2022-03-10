9b2cb08411c0869a54f3867b13faf5a804168d6b
mary <2321493460@qq.com>
2022-03-10 22:13:40 +0800

    source map追踪错误,webpackDevMiddleware,watch,server

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
+  mode: "development",
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  //在开发环境追踪错误。不可用于生产环境
+  devtool: "inline-source-map",
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      // title: '管理输出',
+      title: "Development",
     }),
   ],
   output: {
     // filename: 'main.js',
     // filename: 'bundle.js',
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
