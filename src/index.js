003c5f7c9813e53514f0d9e6c02d923c39d41276
mary <2321493460@qq.com>
2022-03-11 22:36:23 +0800

    babel解析react,jsx

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import App from "./app";
+import ReactDOM from "react-dom";
+ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,61 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.js",
+  mode: 'development',
   plugins: [
     new HtmlWebpackPlugin({
-     title: 'Progressive Web Application',
+      template: './src/index.html',
+      filename: "index.html",
+      title: "Progressive Web Application",
     }),
-   new WorkboxPlugin.GenerateSW({
-     // 这些选项帮助快速启用 ServiceWorkers
-     // 不允许遗留任何“旧的” ServiceWorkers
-     clientsClaim: true,
-     skipWaiting: true,
-   }),
+    new WorkboxPlugin.GenerateSW({
+      // 这些选项帮助快速启用 ServiceWorkers
+      // 不允许遗留任何“旧的” ServiceWorkers
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
   ],
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+          cacheDirectory: true,
+          cacheCompression: false,
+          },
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  //忽略warnings
+  ignoreWarnings: [
+    {
+      module: /module2\.js\?[34]/, // A RegExp
+    },
+    {
+      module: /[13]/,
+      message: /homepage/,
+    },
+    /warning from compiler/,
+    (warning) => true,
+  ],
 };
